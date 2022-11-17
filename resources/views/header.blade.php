749726abd4e07d0428d31776ad99ec94bc9e7acd
htet <htethtetaung556587@gmail.com>
2022-11-18 00:27:32 +0630

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg bg-dark mb-5">
+    <div class="container-fluid">
+        <a class="navbar-brand text-info" href="#">LARA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                <li class="nav-item">
+                    <a class="nav-link active text-white" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Orders</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <div class="input-group rounded">
+                    <input type="search" class="form-control bg-dark" placeholder="Search" aria-label="Search"
+                        aria-describedby="search-addon" />
+                    <span class="input-group-text border-0" id="search-addon">
+                        <i class="fas fa-search text-info"></i>
+                    </span>
+                </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                    <li class="nav-item ms-4">
+                        <a class="nav-link text-warning">Cart(0)</a>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </div>
+</nav>
